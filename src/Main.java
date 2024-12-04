8103e4cce767935adcc36f7dded853510e248351
Eloiza <eloiza.souza@zup.com.br>
2024-12-04 14:38:45 -0300

    feat: add while

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int[] vetor = {1, 2, 3, 4, 5};
-        int i = 0;
+
         System.out.println("Imprime números de um vetor utilizando for");
         for(int i=0; i <vetor.length; i++){
             System.out.print(vetor[i] + " ");
         }
-        System.out.println();
 
-     
+        System.out.println("\nImprime números de um vetor utilizando while");
+        int j = 0;
+        while (j < vetor.length){
+            System.out.print(vetor[j] + " ");
+            j++;
+        }
+
     }
 }

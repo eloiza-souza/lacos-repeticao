d599682fc76958778518ff8a295f87ed75f7749f
Eloiza <eloiza.souza@zup.com.br>
2024-12-04 14:33:47 -0300

    feat: add loop for

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] vetor = {1, 2, 3, 4, 5};
+        int i = 0;
+        System.out.println("Imprime números de um vetor utilizando for");
+        for(int i=0; i <vetor.length; i++){
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println();
+
+     
     }
 }
